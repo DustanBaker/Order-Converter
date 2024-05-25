832e90c00df489ee881e13b92e73410240f8d7de
Dustan Baker <97811633+DustanBaker@users.noreply.github.com>
2024-05-24 23:19:57 -0500

    Adding enhanced error handling and preset output paths.

--- Order_Converter.py
+++ Order_Converter.py
@@ -7,6 +7,8 @@ from tkinter import filedialog
 from tkinter import messagebox
 import customtkinter
 from PIL import Image
+from datetime import datetime
+import os
 
 customtkinter.set_appearance_mode("system")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
@@ -25,14 +27,6 @@ def read_csv_to_dict(input_file):
             data.append(row)
     return data
 
-#function to read a template csv data to a dictionary to be used in the program
-def read_template_csv_to_dict(input_file):
-    template_data = []
-    with open(input_file, 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for row in csv_reader:
-            template_data.append(row)
-    return template_data
 
 def manipulate_USCG_data(input_data):
     manipulated_data = []
@@ -941,7 +935,7 @@ def manipulate_USCG_data(input_data):
             new_row_1['16'] = 'FedEx 2Day w/return lbl'
             new_row_1['20'] = '1'
             new_row_1['21'] = 'FDE'
-            new_row_1['24'] = 'Dusty Baker'
+            new_row_1['23'] = 'Dusty Baker'
             new_row_1['25'] = '791214637'
             manipulated_data.append(new_row_1)
 
@@ -1021,59 +1015,11 @@ def manipulate_USCG_data(input_data):
             new_row_2['25'] = '791214637'
             manipulated_data.append(new_row_2)
 
-
-
-
-
-
-        else:
-
-            # If row 12 does not match the specified values, add an "Error" row
-
-            if row['12'] not in ['SO-001', 'SO-002', 'SO-003', 'SO-004',
-
-                                 'SR-001', 'SR-002', 'SR-003', 'SR-004',
-
-                                 'AO-001', 'AO-002', 'AO-003', 'AO-004',
-
-                                 'AR-001', 'AR-002', 'AR-003', 'AR-004',
-
-                                 'SRR-001', 'SRR-002', 'SRR-003', 'SRR-004',
-
-                                 'SRRL-001', 'SRRL-002', 'SRRL-003', 'SRRL-004',
-
-                                 'SE-001', 'SE-002', 'SE-003', 'SE-004',
-
-                                 'AE-001', 'AE-002', 'AE-003', 'AE-004',
-
-                                 'RE-001', 'RE-002', 'RE-003', 'RE-004',
-
-                                 'K-001', 'K-002', 'K-003', 'K-004',
-
-                                 'SRV-001', 'SRV-002', 'SRV-003', 'SRV-004',
-
-                                 'ARV-001', 'ARV-002', 'ARV-003', 'ARV-004',
-
-                                 'SVDI-001', 'SVDI-002', 'SVDI-003', 'SVDI-004'
-
-                                 ]:
-
-                error_row = row.copy()
-
-                error_row['19'] = 'Error'
-
-                manipulated_data.append(error_row)
-                # Add this line to make error_added a global variable
-                error_added = True
-
-            else:
-
-                manipulated_data.append(row)
-
     return manipulated_data
 
 def manipulate_Terminix_data(input_data):
     manipulated_data = []
+    errors = []
     global error_added
 
     for row in input_data:
@@ -1249,28 +1195,6 @@ def manipulate_Terminix_data(input_data):
             new_row_6['25'] = '177264750'
             manipulated_data.append(new_row_6)
 
-
-
-        else:
-
-            # If row 12 does not match the specified values, add an "Error" row
-
-            if row['12'] not in ['standard','2day','overnight','Item SKU',
-
-                                 ]:
-
-                error_row = row.copy()
-
-                error_row['19'] = 'Error'
-
-                manipulated_data.append(error_row)
-                # Add this line to make error_added a global variable
-                error_added = True
-
-            else:
-
-                manipulated_data.append(row)
-
     return manipulated_data
 
 
@@ -1279,6 +1203,68 @@ def write_csv_from_dict(output_file, output_data, fieldnames):
         csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         csv_writer.writeheader()
         csv_writer.writerows(output_data)
+
+
+def check_additional_commas_within_strings(input_file):
+    with open(input_file, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        header = next(reader)
+        errors = []
+
+        for line_num, row in enumerate(reader, start=2):  # Start from 2 to account for the header row
+            for col_num, value in enumerate(row):
+                # Check if there are commas within the string
+                if ',' in value:
+                    errors.append(f"Error: Comma found in row {line_num}, column {col_num + 1}")
+
+        return errors
+
+def USCG_Error_Handling(input_file):
+    with open(input_file, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        header = next(reader)
+        errors = []
+
+        valid_kit_ids = [
+            'SO-001', 'SO-002', 'SO-003', 'SO-004',
+            'SR-001', 'SR-002', 'SR-003', 'SR-004',
+            'AO-001', 'AO-002', 'AO-003', 'AO-004',
+            'AR-001', 'AR-002', 'AR-003', 'AR-004',
+            'SRR-001', 'SRR-002', 'SRR-003', 'SRR-004',
+            'SRRL-001', 'SRRL-002', 'SRRL-003', 'SRRL-004',
+            'SE-001', 'SE-002', 'SE-003', 'SE-004',
+            'AE-001', 'AE-002', 'AE-003', 'AE-004',
+            'RE-001', 'RE-002', 'RE-003', 'RE-004',
+            'K-001', 'K-002', 'K-003', 'K-004',
+            'SRV-001', 'SRV-002', 'SRV-003', 'SRV-004',
+            'ARV-001', 'ARV-002', 'ARV-003', 'ARV-004',
+            'SVDI-001', 'SVDI-002', 'SVDI-003', 'SVDI-004',
+            'Item SKU', 'Kit ID'
+        ]
+
+        for line_num, row in enumerate(reader, start=2):  # Start from 2 to account for the header rows
+            if len(row) > 11 and row[11] not in valid_kit_ids:
+                errors.append(f"Error: Incorrect Kit ID found in row {line_num}")
+
+        return errors
+
+def Terminix_error_handling(input_file):
+
+    with open(input_file, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        header = next(reader)
+        errors = []
+
+        valid_kit_ids = [
+            'standard', '2day', 'overnight',
+            'Item SKU', 'Kit ID'
+        ]
+
+        for line_num, row in enumerate(reader, start=2):  # Start from 2 to account for the header rows
+            if len(row) > 11 and row[11] not in valid_kit_ids:
+                errors.append(f"Error: Incorrect Kit ID found in row {line_num}")
+
+        return errors
 
 
 def convert_USCG_csv(input_path, output_path):
@@ -1308,52 +1294,7 @@ def convert_Terminix_csv(input_path, output_path):
     write_csv_from_dict(output_path, manipulated_data, fieldnames)
 
 
-
-# create a function that creates a tkinter button that calls the convert_csv function
-def USCG_convert_button_click():
-    global error_added  # Declare error_added as a global variable
-    # Get the input and output file paths
-    input_path = filedialog.askopenfilename(title="Select Input File for USCG", filetypes=[("CSV Files", "*.csv")])
-    output_path = filedialog.asksaveasfilename(title="Select Output File", defaultextension=".csv",
-                                               filetypes=[("CSV Files", "*.csv")])
-
-    # Convert the CSV file
-    convert_USCG_csv(input_path, output_path)
-
-    # Show a success message
-    messagebox.showinfo(title="Semper Paratus", message="Just anther step in the long climb to Liberty!")
-
-    # if error_added is True, show an error message
-    if error_added:
-        show_error_with_image()
-
-    # Reset error_added to False
-    error_added = False
-
-# create a function that creates a tkinter button that calls the convert_csv function for Terminix
-def Terminix_convert_button_click():
-    global error_added  # Declare error_added as a global variable
-    # Get the input and output file paths
-    input_path = filedialog.askopenfilename(title="Select Input File for Terminix", filetypes=[("CSV Files", "*.csv")])
-    output_path = filedialog.asksaveasfilename(title="Select Output File", defaultextension=".csv",
-                                               filetypes=[("CSV Files", "*.csv")])
-
-    # Convert the CSV file
-    convert_Terminix_csv(input_path, output_path)
-
-    # Show a success message
-    messagebox.showinfo(title="Successful conversion", message="Just anther step in the long climb to Liberty!")
-
-    # if error_added is True, show an error message
-    if error_added:
-        show_error_with_image()
-
-    # Reset error_added to False
-    error_added = False
-
-# create a function that creates a tkinter button that calls the convert_csv function for VA HARDWARE
-
-
+# create a function that shows an error message with an image
 def show_error_with_image():
     # Create a new top-level window
     error_window = customtkinter.CTkToplevel()
@@ -1366,6 +1307,98 @@ def show_error_with_image():
     error_label.pack(side='top', pady=20)
     error_message = customtkinter.CTkLabel(error_window, text="Womp Womp...Errors have been detected. \nPlease review the CSV file.")
     error_message.pack(side='top', pady=20)
+
+
+# create a function that creates a tkinter button that calls the convert_csv function
+def USCG_convert_button_click():
+    global error_added  # Declare error_added as a global variable
+    # Get the input file path
+    input_path = filedialog.askopenfilename(title="Select Input File for USCG", filetypes=[("CSV Files", "*.csv")])
+
+    # Check for additional commas within strings
+    errors = check_additional_commas_within_strings(input_path)
+    if errors:
+        messagebox.showerror(title="CSV Error", message="\n".join(errors))
+        return
+
+    # Check for correct USCG kit ID
+    errors = USCG_Error_Handling(input_path)
+    if errors:
+        messagebox.showerror(title="CSV Error", message="\n".join(errors))
+        return
+
+    # Create the default output file path
+    current_date = datetime.now().strftime("%m-%d-%Y")
+    default_directory = r"T:\3PL Files\Shipment Template"
+    default_filename = f"USCG Shipment {current_date}.csv"
+    default_output_path = os.path.join(default_directory, default_filename)
+
+    try:
+        # Attempt to save to the default path
+        convert_USCG_csv(input_path, default_output_path)
+        messagebox.showinfo(title="Semper Paratus", message=f"File saved successfully at {default_output_path}")
+    except (OSError, IOError):
+        # If the default path is unavailable, ask the user for the output directory and base filename
+        output_base_path = filedialog.asksaveasfilename(
+            title="Select Output Directory and Base Filename",
+            defaultextension=".csv",
+            filetypes=[("CSV Files", "*.csv")],
+            initialfile=default_filename
+        )
+
+        if not output_base_path:
+            return  # User cancelled the save dialog
+
+        # Save to the selected path
+        convert_USCG_csv(input_path, output_base_path)
+        messagebox.showinfo(title="Semper Paratus", message=f"File saved successfully at {output_base_path}")
+
+
+# create a function that creates a tkinter button that calls the convert_csv function for Terminix
+def Terminix_convert_button_click():
+    global error_added  # Declare error_added as a global variable
+    # Get the input and output file paths
+    input_path = filedialog.askopenfilename(title="Select Input File for Terminix", filetypes=[("CSV Files", "*.csv")])
+
+    # Check for additional commas within strings
+    errors = check_additional_commas_within_strings(input_path)
+    if errors:
+        messagebox.showerror(title="CSV Error", message="\n".join(errors))
+        return
+
+    # Check for correct Terminix kit ID
+    errors = Terminix_error_handling(input_path)
+    if errors:
+        messagebox.showerror(title="CSV Error", message="\n".join(errors))
+        return
+        # Create the default output file path
+    current_date = datetime.now().strftime("%m-%d-%Y")
+    default_directory = r"T:\3PL Files\Shipment Template"
+    default_filename = f"Terminix Shipment {current_date}.csv"
+    default_output_path = os.path.join(default_directory, default_filename)
+
+    try:
+        # Attempt to save to the default path
+        convert_USCG_csv(input_path, default_output_path)
+        messagebox.showinfo(title="Semper Paratus", message=f"File saved successfully at {default_output_path}")
+    except (OSError, IOError):
+        # If the default path is unavailable, ask the user for the output directory and base filename
+        output_base_path = filedialog.asksaveasfilename(
+            title="Select Output Directory and Base Filename",
+            defaultextension=".csv",
+            filetypes=[("CSV Files", "*.csv")],
+            initialfile=default_filename
+        )
+
+        if not output_base_path:
+            return  # User cancelled the save dialog
+
+        # Save to the selected path
+        convert_USCG_csv(input_path, output_base_path)
+        messagebox.showinfo(title="Sweet Liberty!", message=f"File saved successfully at {output_base_path}")
+
+
+
 
 
 
